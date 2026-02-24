f32ae2d7bda3ca8e7a2f3c66c4c73000505814bb
Hritwik-Biswas <hritwikofficial2025@gmail.com>
2026-02-24 12:15:20 +0530

    Fix Some Error

--- Snake-Water-Gun-Game.py
+++ Snake-Water-Gun-Game.py
@@ -1,13 +1,20 @@
 import random
 
 computer=random.choice([-1,0,1])
-userstr = input("Enter Your Choise: ")
+
+userstr = input("Enter Your Choice(s/w/g): ").lower()
+
 userDict = {'s':-1,'w':0,'g':1}
 viewDict = {-1:'Snake',0:'Water',1:'Gun'}
 
-user = userDict[userstr]
+user = userDict.get(userstr)
 
-print(f'User Choose: {viewDict[user]}\nComputer Choose: {viewDict[computer]}')
+if user is None:
+    print("Invalid input! Please enter s, w, or g.")
+    exit()
+
+print(f'User Choose: {viewDict[user]}')
+print(f'Computer Choose: {viewDict[computer]}')
 
 if(computer==user):
     print('It is DRAW')
